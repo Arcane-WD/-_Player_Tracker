443c66a903186a19ff9f9ebe70263b4caffe0043
ArcaneWD <harsha2004alpur@gmail.com>
2025-07-09 16:31:35 +0530

    Initial commit: setup project structure with uv venv

---
+++ main.py
@@ -0,0 +1,10 @@
+from utils import read_video, save_video
+import cv2
+import numpy as np
+
+def main():
+    video_frames = read_video("videos\input\inputvid.mp4")
+
+
+if __name__ == "__main__":
+    main()
