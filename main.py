512d0acf62fd5aba3725c6b9cb7e2bc091eac4a5
ArcaneWD <harsha2004alpur@gmail.com>
2025-07-11 00:17:39 +0530

    Test YOLOv11 model on sample video with GPU support

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from utils import read_video, save_video
 import cv2
 import numpy as np
-
+from ultralytics import YOLO
+import torch
 def main():
-    video_frames = read_video("videos\input\inputvid.mp4")
+    video_frames = read_video(r"videos/input/inputvid.mp4")
 
+    # Run video frames through model on gpu
+    model = YOLO(r"model/best.pt")
+    model.to(torch.device('cuda'))
 
+    output_video_frames = []
+    for i, frames in enumerate(video_frames):
+        result = model.predict(frames, device='cuda', verbose=False)[0]
+        output_frame = result.plot()
+        output_video_frames.append(output_frame)
+        print(f"Frame {i+1} out of {len(video_frames)}")
+
+    #Save Video Frames
+    save_video(output_video_frames, "videos\output\outputvid.avi")
 if __name__ == "__main__":
     main()
